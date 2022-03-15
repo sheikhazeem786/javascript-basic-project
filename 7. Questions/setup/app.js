73e440405e67b309619116400059ba261256def0
sheikhazeem786 <sheikhazeem141@gmail.com>
2022-03-15 23:56:34 +0530

    adding upto css and js of questions

--- 7. Questions/setup/app.js
+++ 7. Questions/setup/app.js
@@ -1,2 +1,11 @@
 //using selectors inside the element
 // traversing the dom
+
+const btns = document.querySelectorAll(".question-btn");
+
+btns.forEach(function(btn) {
+    btn.addEventListener("click", function(e) {
+        console.log(e.currentTarget);
+    })
+
+});
