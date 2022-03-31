eed73e2a126f8e940b66165fb17148ae0ffcd345
sheikhazeem786 <sheikhazeem141@gmail.com>
2022-03-31 13:02:10 +0530

    adding "scroll" project

--- 10. Scroll/setup/app.js
+++ 10. Scroll/setup/app.js
@@ -4,10 +4,71 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
-
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
 // ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
 
+navToggle.addEventListener("click", function() {
+    // linkContainer.classList.toggle("show-links");
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+    // console.log(linksHeight);
+    if (containerHeight === 0) {
+        linksContainer.style.height = `${linksHeight}px`;
+    } else {
+        linksContainer.style.height = 0;
+    }
+});
+const navBar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
 // ********** fixed navbar ************
+window.addEventListener("scroll", function() {
+        const scrollHeight = window.pageYOffset;
+        // console.log(window.pageYOffset);
+        const navHeight = navBar.getBoundingClientRect().height;
+        if (scrollHeight > navHeight) {
+            navBar.classList.add("fixed-nav");
+        } else {
+            navBar.classList.remove("fixed-nav");
+        }
 
-// ********** smooth scroll ************
-// select links
+        if (scrollHeight > 500) {
+            topLink.classList.add("show-link");
+        } else {
+            topLink.classList.remove("show-link");
+        }
+    })
+    // ********** smooth scroll ************
+    // select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(function(link) {
+    link.addEventListener("click", function(e) {
+        // prevent default
+        e.preventDefault();
+        // navigate to specific spot
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        // console.log(id);
+        const element = document.getElementById(id);
+        // calculate the height
+        const navHeight = navBar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navBar.classList.contains("fixed-nav");
+        let position = element.offsetTop - navHeight;
+        if (!fixedNav) {
+            position = position - navHeight;
+        }
+        if (navHeight > 82) {
+            position = position + containerHeight;
+        }
+        // console.log(position);
+        window.scrollTo({
+            left: 0,
+            top: position,
+        });
+        linksContainer.style.height = 0;
+    });
+});
